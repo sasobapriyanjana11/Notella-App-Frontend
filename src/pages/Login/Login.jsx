dc294b71f88250f8c3a4d864d20f38a03e55c3de
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2025-03-14 20:07:00 +0530

    create navigation components to signin ,signup and dashboard

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from "../../components/Navbar/Navbar.jsx";
+
+const Login = () => {
+    return <>
+        <Navbar/>
+        <div>
+            <div>
+                <form onSubmit={()=>{}}></form>
+                <h4 className="text-2xl mb-7">Login</h4>
+                <input type="text" placeholder="Email" className="input-box" />
+                <button type={y}></button>
+            </div>
+        </div>
+    </>
+
+
+}
+export default Login
