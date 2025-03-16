4dc5208b681f14ca78160753470018ff1f280ed2
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2025-03-16 17:38:08 +0530

    add notell app screen shots

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -24,7 +24,7 @@ const NavBar = ({userInfo,onSearchNote,handleClearSearch}) => {
     return (
         <div className="bg-white flex items-center justify-between px-6 py-2 shadow-md">
             <h2 className="text-xl font-medium text-black py-2">
-                Notes
+                @ Notella ~
             </h2>
             <SearchBar
                 value={searchQuery}
