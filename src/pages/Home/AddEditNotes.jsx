6f0351b7539c8bf70f2dd909f487c80401797063
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2025-03-16 10:22:04 +0530

    create tagInput component

--- src/pages/Home/AddEditNotes.jsx
+++ src/pages/Home/AddEditNotes.jsx
@@ -1,13 +1,55 @@
-import React from 'react'
-const AddEditNotes=()=>{
+import React, {useState} from 'react'
+import TagInput from "../../components/Input/TagInput.jsx";
+import {MdClose} from "react-icons/md";
+const AddEditNotes=({noteData,type,onClose})=>{
+
+    const[title, setTitle] = useState("");
+    const[content, setContent] = useState("");
+    const[tags, setTags] = useState([]);
+
+    const[error, setError] = useState(null);
+
+    //add a note
+    const addNewNote = async ()=>{};
+
+    //edit note
+    const editNote = async ()=>{};
+
+    const handleAddNote=()=>{
+        if(!title){
+            setError("Pleace enter the  title");
+            return;
+        }
+        if(!content){
+            setError("Pleace enter the  content");
+            return;
+        }
+        setError("");
+
+        if(type ==="edit"){
+            editNote();
+        }else{
+            addNewNote();
+        }
+    };
+
     return (
-        <div>
+        <div className="relative">
+            <button
+                className="w-10 h-10 rounded-full flex items-center justify-center absolute -top-3 -right-3 hover:bg-slate-50"
+                onClick={onClose}
+            >
+             <MdClose className="text-xl text-slate-400"/>
+            </button>
+
             <div className="flex flex-col gap-2">
                 <label className="input-label">TITLE</label>
                 <input
                     type="text"
                     className="text-2xl text-slate-950 outline-none "
                     placeholder="Go to Gym at 5 AM"
+                    value={title}
+                    onChange={({target}) => setTitle(target.value)}
                 />
             </div>
             <div className="flex flex-col gap-2 mt-4">
@@ -17,16 +59,23 @@ const AddEditNotes=()=>{
                     className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
                     placeholder="Content"
                     rows={10}
+                    value={content}
+                    onChange={({target}) => setContent(target.value)}
 
-               />
+                />
 
             </div>
             <div className="mt-3">
                 <label className="input-label">Tags</label>
-                <button className="btn-primary font-medium mt-5 p-3" onClick={()=>{}}>
-                    ADD
-                </button>
+                <TagInput tags={tags} setTags={setTags}/>
             </div>
+
+            {error && <p className="text-red-500 text-xs pt-4">{error}</p>}
+
+            <button className="btn-primary font-medium mt-5 p-3" onClick={handleAddNote}>
+                ADD
+            </button>
+
         </div>
     )
 }
