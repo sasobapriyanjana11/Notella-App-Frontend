14e0e77ad367dc9fd7ebf375fba999d8c411e0a4
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2025-03-16 15:28:09 +0530

    implement delete note to delete note card

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -76,6 +76,23 @@ const Home = () => {
         }
     };
 
+    // delete note
+    const deleteNote = async (data) => {
+        const noteId=data._id;
+        try{
+            const response=await axiosInstance.delete("/delete-note/"+noteId);
+            if(response.data && !response.data.error){
+                showToastMessage("Note Deleted Successfully!",'delete');
+                getAllNotes()
+
+            }
+        }catch(error){
+            if(error.response && error.response.data && error.response.data.message){
+                console.log("An unexpected error occurred. Try again later.");
+            }
+        }
+    }
+
     useEffect(() => {
         getAllNotes();
         getUserInfo();
@@ -95,7 +112,7 @@ const Home = () => {
                             tags={item.tags}
                             isPinned={item.isPinned}
                             onEdit={() =>handleEdit(item)}
-                            onDelete={() => {}}
+                            onDelete={() => deleteNote(item)}
                             onPinNote={() => {}}
                         />
                     ))}
